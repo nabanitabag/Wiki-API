4cef0e976e399d0e54b9329a732413dc570757b6
Nabanita Bag <ashley.makruz@gmail.com>
2021-03-06 13:29:05 +0530

    init file

---
+++ app.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("article", articleSchema);
+
+app.get("/articles", function (req, res) {
+  Article.find(function (err, foundArticles) {
+    res.send(foundArticles);
+  })
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
